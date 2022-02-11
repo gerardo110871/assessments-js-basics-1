a6a70009db11c07c1f11ab54991de574560a493b
Gerardo <gpadil@icloud.com>
2022-02-11 12:24:41 -0700

    hopefully this is passing

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 
 let user = prompt("Welcome, Please validate password here")
 
-} if (user >= 10){
+if (user.length >= 10){
     console.log("password accepted")
 } else {
     console.log ("password is too short")
